ac8d252b9dd1623339b4b31545ca5759f051d6e3
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-11 11:53:26 +0200

    useragent: Updated the fallback UA

--- UserAgentFetcher.cs
+++ UserAgentFetcher.cs
@@ -27,7 +27,7 @@ namespace NuciWeb.HTTP
                 return match.Value;
             }
 
-            return "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0";
+            return "Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0";
         }
     }
 }
